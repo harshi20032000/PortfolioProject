6ef513bb9661148b07ffc3812440ab306fb5da4a
Harshikesh <harshibabu604@gmail.com>
2025-04-28 21:10:51 +0530

    product-details and images corrected

--- src/app/constants/product-images.ts
+++ src/app/constants/product-images.ts
@@ -1,16 +1,110 @@
 export const PRODUCT_IMAGES = [
     {
-        "productID": "MSECOFLEXI",
+        "productID": "MSLOADER",
         "productColorHex": "#FF0000",
         "pictures": [
-          "https://res.cloudinary.com/dyizlmke8/image/upload/v1719643733/Movestone/Products_new/ECO%20FLEXI/SEN_3544_iubiuy.jpg"
+            "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827798/LOADER_red2_zfhlqp.webp",
+            "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827797/LOADER_red1_bupbsa.webp",
+            "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827800/LOADER_red3_ifsqbo.webp",
+            "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827801/LOADER_red4_nbwyuy.webp"
         ]
-      },
-      {
-        "productID": "MSECOFLEXI",
-        "productColorHex": "#A9A9A9",
-        "pictures": [
-          "https://res.cloudinary.com/dyizlmke8/image/upload/v1719643721/Movestone/Products_new/ECO%20FLEXI/SEN_3571_xp3r11.jpg"
-        ]
-      }
+    },
+    {
+      "productID": "MSDELUXE",
+      "productColorHex": "#2E8B57",
+      "pictures": [
+          "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827756/DELUXE_green_qelqeu.webp"
+      ]
+  }, 
+  {
+    "productID": "MSDELUXE",
+    "productColorHex": "#C0C0C0",
+    "pictures": [
+        "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827754/DELUXE_blue_axjncx.webp"
+    ]
+},
+{
+  "productID": "MSDELUXE",
+  "productColorHex": "#0000FF",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827752/DELUXE_deepgreen_vq3jjp.webp"
+  ]
+},
+{
+  "productID": "MSECOFLEXI",
+  "productColorHex": "#FF0000",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827680/ECOFLEXI_red_qrsrnu.webp"
+  ]
+},
+{
+  "productID": "MSECOFLEXI",
+  "productColorHex": "#A9A9A9",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827680/ECOFLEXI_grey_s1ezqn.webp"
+  ]
+},
+{
+  "productID": "MSSUPERPLUS",
+  "productColorHex": "#FF0000",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827597/SUPERPLUS_red_cgmzce.webp"
+  ]
+},
+{
+  "productID": "MSSUPERPLUS",
+  "productColorHex": "#0000FF",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827597/SUPERPLUS_blue_y3uf9f.webp"
+  ]
+},
+{
+  "productID": "MSMAXFLEXI",
+  "productColorHex": "#C0C0C0",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827634/MAXFLEXI_grey_z5hwuy.webp"
+  ]
+},
+{
+  "productID": "MSMAXFLEXI",
+  "productColorHex": "#FF0000",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827636/MAXFLEXI_red_ejrfxw.webp"
+  ]
+},
+{
+  "productID": "MSMAXFLEXI",
+  "productColorHex": "#0000FF",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827635/MAXFLEXI_blue_fncf1n.webp"
+  ]
+},
+{
+  "productID": "MSECOSUPER",
+  "productColorHex": "#008000",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827721/ECOSS_green_r4z9az.webp"
+  ]
+},
+{
+  "productID": "MSECOSUPER",
+  "productColorHex": "#FFD700",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827729/ECOSS_yellow_bfbqhf.webp"
+  ]
+},
+{
+  "productID": "MSECOSUPER",
+  "productColorHex": "#0000FF",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827719/ECOSS_blue_jvc0ce.webp"
+  ]
+},
+{
+  "productID": "MSECOSUPER",
+  "productColorHex": "#A9A9A9",
+  "pictures": [
+      "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827718/ECOSS_grey_nsoi7p.webp"
+  ]
+}
 ]
