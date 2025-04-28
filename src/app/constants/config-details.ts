eef89053fee9f78098024303818cd30d0d409e57
Harshikesh <harshibabu604@gmail.com>
2025-04-28 15:53:39 +0530

    config-details corrected

--- src/app/constants/config-details.ts
+++ src/app/constants/config-details.ts
@@ -5,27 +5,27 @@ export const CONFIG_DETAILS = {
         {
             "pageName": "home",
             "sectionName": "banner",
-            "attribute1": "https://res.cloudinary.com/dyizlmke8/image/upload/v1719644023/Movestone/Home%20page/Slider_1_ngblpg.jpg"
+            "attribute1": "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827512/Slider_1_ocvotl.webp"
         },
         {
             "pageName": "home",
             "sectionName": "banner",
-            "attribute1": "https://res.cloudinary.com/dyizlmke8/image/upload/v1719644024/Movestone/Home%20page/Slider_2_atdkwv.jpg"
+            "attribute1": "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827512/Slider_2_e1lhgu.webp"
         },
         {
             "pageName": "home",
             "sectionName": "banner",
-            "attribute1": "https://res.cloudinary.com/dyizlmke8/image/upload/v1719644024/Movestone/Home%20page/Slider_3_oydwm1.jpg"
+            "attribute1": "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827512/Slider_3_zvimia.webp"
         },
         {
             "pageName": "home",
             "sectionName": "banner",
-            "attribute1": "https://res.cloudinary.com/dyizlmke8/image/upload/v1719644026/Movestone/Home%20page/Slider_4_nguptf.jpg"
+            "attribute1": "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827512/Slider_4_l36wpp.webp"
         },
         {
             "pageName": "home",
             "sectionName": "banner",
-            "attribute1": "https://res.cloudinary.com/dyizlmke8/image/upload/v1719644027/Movestone/Home%20page/Slider_5_adural.jpg"
+            "attribute1": "https://res.cloudinary.com/dkoftgwba/image/upload/v1745827512/Slider_5_m5ya0s.webp"
         }
     ]
 }
